f891d33ecc0668a67c91b99129c149cd616b1863
EvoluteStudio <101565326+EvoluteStudio@users.noreply.github.com>
2022-03-21 19:29:52 +0800

    20220321

--- evolute_ctl.py
+++ evolute_ctl.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
 import argparse
+import importlib
 import os
 import os.path
 import shutil
-
+import evolute_params
 parser = argparse.ArgumentParser(description='customize system args')
 parser.add_argument('--run', '-r', help='run system', const=True, nargs='?')
 parser.add_argument('--update', '-u', help='update system', const=True, nargs='?')
 parser.add_argument('--restart', '-rs', help='restart system', const=True, nargs='?')
 parser.add_argument('--kill', '-k', help='kill system', const=True, nargs='?')
 parser.add_argument('--status', '-s', help='check system status', const=True, nargs='?')
+parser.add_argument('--rollback', '-rb', help='rollback system', const=True, nargs='?')
 
 yml_path = './docker-compose.yml'
 github_base = 'https://github.com/EvoluteStudio/evolute-onekeyrun'
 
 
-def replace_sample(version):
-    # importlib.reload(evolute_params)
-    from evolute_params import replace_params
-    try:
-        status = replace_params(version)
-    except Exception as e:
-        print(f'配置文件写入失败，请检查后重新启动： {str(e)}')
-        return False
-    if status:
-        print('配置文件写入完成...')
-    return True
-    # 修改yml文件里面对应的内容
-    # 先检查配置是否齐全
-
 def github_download():
+    local_version = '20200202'
     rets = os.popen(
         f"curl {github_base}/releases/latest | grep -Eo '[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+'")
     new_version = ''
@@ -39,13 +28,13 @@ def github_download():
     if not new_version:
         print(f'未获取到版本内容，请前往{github_base}/releases/，下载压缩包尝试手动安装>(⊙﹏⊙)<')
         print('Bye~')
-        return False
+        return False, False
     if os.path.exists('./.version'):
         with open('.version', 'r') as f:
             local_version = f.readline()
         if local_version >= new_version:
             print('当前已是最新版本~(￣▽￣)~')
-            return False
+            return False, False
     print('开始更新...')
     # 下载压缩包、替换模板文件、重启
     DOWNLOAD_URL = f"wget --no-check-certificate {github_base}/archive/refs/tags/" + new_version + ".tar.gz"
@@ -53,12 +42,13 @@ def github_download():
         os.system(DOWNLOAD_URL)
     if not os.path.exists(f'{new_version}.tar.gz'):
         print(f'下载更新包失败>(⊙﹏⊙)<，可以尝试手动下载tar.gz压缩包后安装更新: {DOWNLOAD_URL}')
-        return False
+        return False, False
     os.system(f"tar zxvf {new_version}.tar.gz")
-    new_version_dir = f'test_version-{new_version[1:]}'
+    new_version_dir = f'evolute-onekeyrun-{new_version}'
     cwd = os.getcwd()
     if not os.path.exists(new_version_dir):
         print(f'找不到文件夹: {new_version_dir}')
+
     for root, dirs, files in os.walk(new_version_dir):
         for file in files:
             if 'evolute_ctl' in file:
@@ -74,108 +64,36 @@ def github_download():
         os.remove(f'{new_version}.tar.gz')
     return local_version, new_version
 
-
-
-def run_system():
-    # 检查一下是否已经有在启动
-    install_check = check_system()
-    if not install_check:
-        print('开始安装...')
-        local_version, new_version = github_download()
-        if not new_version:
-            skip_permission = input("获取版本信息失败，是否跳过此次更新直接启动：y/n？")
-            if isinstance(skip_permission, str) and skip_permission.startswith('y'):
-                print('...')
-            else:
-                return
-        status = replace_sample(new_version)
-        if not status:
-            return
-        from evolute_params import restart_system
-        restart_system(False)
-        with open('.version', 'w') as f:
-            f.write(new_version)
-        from evolute_params import run_install_scripts
-        run_install_scripts()
-        print('安装成功~')
-    else:
-        update_flag = input('检测到您本地已安装相关服务，是否进行更新：y/n?')
-        if isinstance(update_flag, str) and update_flag.startswith('y'):
-            print('开始更新...')
-            update_system()
-        print('退出...')
-        return
-
-
-def update_system():
-    install_check = check_system()
-    if install_check:
-        local_version, new_version = github_download()
-        if not new_version:
-            return
-        status = replace_sample(new_version)
-        if not status:
-            return
-        print('开始备份数据...')
-        from evolute_params import backup_data
-        try:
-            backup_flag = backup_data()
-        except Exception as e:
-            backup_flag = False
-        if not backup_flag:
-            skip_backup = input('备份数据失败，是否跳过备份直接更新：y/n?')
-            if isinstance(skip_backup, str) and skip_backup.startswith('y'):
-                print('开始更新...')
-            else:
-                return
-        from evolute_params import restart_system
-        restart_system(True)
-        from evolute_params import run_update_scripts
-        result = run_update_scripts()
-        if not result:
-            if backup_flag:
-                from evolute_params import restore_data
-                restore_data()
-                print('脚本执行异常，自动回退到上个版本...')
-                from evolute_params import restart_system
-                restart_system()
-        with open('.version', 'w') as f:
-            f.write(new_version)
-        print('更新完成~')
-    else:
-        print('Evolute服务未安装或未启动...')
-        install_permission = input("是否进行更新重启：y/n？")
-        if isinstance(install_permission, str) and install_permission.startswith('y'):
-            run_system()
-        else:
-            print('退出...')
-
-
-def kill_system():
-    os.system(f'docker-compose down')
-
-
-def system_status():
-    res = os.popen('docker-compose ps')
-    if not res:
-        print('服务未启动~')
-        return
-    for line in res.readlines():
-        print(line)
-
-
 if __name__ == '__main__':
     # 先检查是否有配置好的文件
+    install_check = evolute_params.check_system()
     args = parser.parse_args()
     if args.run:
-        run_system()
+        if not install_check:
+            local_version, new_version = github_download()
+            importlib.reload(evolute_params)
+            from evolute_params import run_system
+            run_system(local_version, new_version)
+        else:
+            print('检测到您本地已安装相关服务, 退出安装...')
     if args.status:
+        from evolute_params import system_status
         system_status()
     if args.update:
-        update_system()
+        if install_check:
+            local_version, new_version = github_download()
+            importlib.reload(evolute_params)
+            from evolute_params import update_system
+            update_system(local_version, new_version)
+        else:
+            print('Evolute服务未安装或未启动...')
     if args.restart:
-        from evolute_params import restart_system, check_system
+        from evolute_params import restart_system
         restart_system()
     if args.kill:
+        from evolute_params import kill_system
         kill_system()
+    if args.rollback:
+        from evolute_params import rollback_system
+        rollback_system()
 
--- evolute_params.py
+++ evolute_params.py
@@ -6,8 +6,8 @@ import os
 import os.path
 import shutil
 
-BACKUP_DB = False
-BACKUP_MEDIA = False
+BACKUP_DB = True
+BACKUP_MEDIA = True
 BACKUP_ES = False
 
 parser = argparse.ArgumentParser(description='customize system args')
@@ -22,8 +22,8 @@ parser.add_argument('--kill', '-k', help='kill system', const=True, nargs='?')
 parser.add_argument('--check', '-c', help='check system', const=True, nargs='?')
 parser.add_argument('--websocket_domain', '-wd', help='set websocket_domain')
 parser.add_argument('--websocket_port', '-op', help='set websocket_port')
-parser.add_argument('--gunicorn_worker','-gw', help='set gunicoren worker')
-parser.add_argument('--celery_worker','-cw', help='set celery worker')
+parser.add_argument('--gunicorn_worker', '-gw', help='set gunicoren worker')
+parser.add_argument('--celery_worker', '-cw', help='set celery worker')
 success_path = './docker/qawiki/volume/logs/success.txt'
 
 #
@@ -100,20 +100,23 @@ def set_op(op):
         old_settings['EVOLUTE_WEBSOCKET_PORT'] = op
         json.dump(old_settings, new_settings)
 
+
 def set_gw(gw):
     with open(settings_path, 'r', encoding='utf8') as o_settings:
         old_settings = json.load(o_settings)
     with open(settings_path, 'w', encoding='utf8') as new_settings:
-        #old_settings['GUNICORN_WORKER'] = gw
+        # old_settings['GUNICORN_WORKER'] = gw
         old_settings.update({'GUNICORN_WORKER': gw})
         json.dump(old_settings, new_settings)
+
 
 def set_cw(cw):
     with open(settings_path, 'r', encoding='utf8') as o_settings:
         old_settings = json.load(o_settings)
     with open(settings_path, 'w', encoding='utf8') as new_settings:
-        old_settings.update({'CELERY_WORKER':cw})
+        old_settings.update({'CELERY_WORKER': cw})
         json.dump(old_settings, new_settings)
+
 
 def replace_params(image_tag):
     local_version = '20200202'
@@ -144,15 +147,22 @@ def replace_params(image_tag):
     with open(yml_path, 'w', encoding='utf-8') as yml_settings_content:
         # 替换镜像tag
         yml_settings['services']['evolute']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-deploy:' + image_tag
-        yml_settings['services']['evolute-studio']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-studio:' + image_tag
-        yml_settings['services']['celery-board']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
-        yml_settings['services']['evolute-board']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
-        yml_settings['services']['celery-board-beat']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
-        yml_settings['services']['evolute-wiki']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
-        yml_settings['services']['evolute-wiki-ws']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
-        yml_settings['services']['celery-wiki-beat']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
-        yml_settings['services']['celery-wiki']['image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
-
+        yml_settings['services']['evolute-studio'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-studio:' + image_tag
+        yml_settings['services']['celery-board'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
+        yml_settings['services']['evolute-board'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
+        yml_settings['services']['celery-board-beat'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-board:' + image_tag
+        yml_settings['services']['evolute-wiki'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
+        yml_settings['services']['evolute-wiki-ws'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
+        yml_settings['services']['celery-wiki-beat'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
+        yml_settings['services']['celery-wiki'][
+            'image'] = 'ncr-partner.nie.netease.com/evolute/evolute-wiki:' + image_tag
 
         # 修改对应域名和端口
         yml_settings['services']['celery-board']['environment']['SUFFIX'] = settings['EVOLUTE_EVERTEST_DOMAIN']
@@ -216,6 +226,7 @@ def replace_params(image_tag):
         nginx_content.write(nginx_settings)
     return True
 
+
 def restart_system(update=False):
     if update:
         try:
@@ -232,8 +243,8 @@ def restart_system(update=False):
     # res = os.popen('docker-compose ps')
     # for line in res.readlines():
     #     print(line)
-        # if 'evolute' in line or 'board' in line or 'wiki' in line:
-        #     print(line)
+    # if 'evolute' in line or 'board' in line or 'wiki' in line:
+    #     print(line)
 
     flag = True
     print('服务启动中，请稍等 ...')
@@ -243,36 +254,47 @@ def restart_system(update=False):
         else:
             flag = False
 
+
 def backup_data(version):
     # 备份数据库
     if BACKUP_DB:
         if os.path.exists('./docker/mysql-base'):
             shutil.rmtree('./docker/mysql-base')
         if os.path.exists('./docker/mysql'):
-            shutil.copy('./docker/mysql', f'./docker/mysql-base')
+            shutil.copytree('./docker/mysql', f'./docker/mysql-base')
     # 备份文件
     if BACKUP_MEDIA:
         if os.path.exists('./docker/evolute-studio-base'):
             shutil.rmtree('./docker/evolute-studio-base')
         if os.path.exists('./docker/evolute-studio'):
-            shutil.copy('./docker/evolute-studio', f'./docker/evolute-studio-base')
+            shutil.copytree('./docker/evolute-studio', f'./docker/evolute-studio-base')
     if BACKUP_ES:
-        res = os.popen("docker exec -it evolute-wiki /bin/bash -c 'curl -XGET  evolute_es01:9200/_snapshot/evolute_backup'")
-        backup_json = '{"type": "fs","settings": {"location": "/usr/share/elasticsearch/backup"}}'
-        backup_cmd = f"curl -H 'Content-Type: application/json' -s -XPUT  evolute_es01:9200/_snapshot/evolute_backup -d {backup_json}"
-        os.system(f"docker exec -it evolute-wiki /bin/bash -c {backup_cmd}")
-        os.system(f"docker exec -it evolute-wiki /bin/bash -c 'curl -XPUT evolute_es01:9200/_snapshot/evolute_backup/snapshot_{version}?wait_for_completion=true'")
+        os.system("docker exec -it evolute-wiki /bin/bash -c './backup_es.sh'")
+        # res = os.popen(
+        #     "docker exec -it evolute-wiki /bin/bash -c 'curl -XGET  evolute_es01:9200/_snapshot/evolute_backup'")
+        # backup_json = '{"type": "fs","settings": {"location": "/usr/share/elasticsearch/backup"}}'
+        # backup_cmd = """curl -H 'Content-Type: application/json' -s -XPUT  evolute_es01:9200/_snapshot/evolute_backup -d '{"type": "fs","settings": {"location": "/usr/share/elasticsearch/backup"}}'"""
+        # os.system(backup_cmd)
+        # os.system(
+        #     f"docker exec -it evolute-wiki /bin/bash -c 'curl -XPUT evolute_es01:9200/_snapshot/evolute_backup/snapshot_{version}?wait_for_completion=true'")
     if os.path.exists('./docker-compose.yml.backup'):
         os.remove('./docker-compose.yml.backup')
-        shutil.copy('./docker-compose.yml', './docker-compose.yml.backup')
+    shutil.copy('./docker-compose.yml', './docker-compose.yml.backup')
+    print('备份完成')
     return True
     # 备份es
 
+
 def restore_data(version):
     # 恢复数据库
+    print('即将回退到上个版本...')
     if os.path.exists('./docker-compose.yml.backup'):
-        os.remove('./docker-compose.yml')
-        os.rename('./docker-compose.yml.backup', './docker-compose.yml')
+        os.system('docker-compose down')
+        if os.path.exists('./docker-compose.yml'):
+            os.remove('./docker-compose.yml')
+        os.rename('docker-compose.yml.backup', 'docker-compose.yml')
+    else:
+        print('无法回退至上一个版本')
     if BACKUP_DB:
         if os.path.exists(f'./docker/mysql-base'):
             shutil.rmtree('./docker/mysql')
@@ -283,8 +305,10 @@ def restore_data(version):
             shutil.rmtree('./docker/evolute-studio')
             os.rename(f'./docker/evolute-studio-base', f'./docker/evolute-studio')
     # 恢复es数据
-    if BACKUP_ES:
-        os.system(f"docker exec -it evolute-wiki /bin/bash -c 'curl -XPOST evolute_es01:9200/_snapshot/evolute_backup/snapshot_{version}/_restore?wait_for_completion=true'")
+    if BACKUP_ES and version:
+        os.system(
+            f"docker exec -it evolute-wiki /bin/bash -c 'curl -XPOST evolute_es01:9200/_snapshot/evolute_backup/snapshot_{version}/_restore?wait_for_completion=true'")
+
 
 def check_system():
     install_check = False
@@ -295,8 +319,14 @@ def check_system():
             return install_check
     return install_check
 
+
 def run_install_scripts():
     return
+
+
+def remove_images(version=''):
+    cmd = f"docker images|grep ncr-partner.nie.netease.com|grep {version}|" + "awk '{print $3}'|xargs docker rmi"
+    os.system(cmd)
 
 
 def run_update_scripts():
@@ -319,6 +349,125 @@ def run_update_scripts():
     return True
 
 
+def replace_sample(version):
+    try:
+        status = replace_params(version)
+    except Exception as e:
+        print(f'配置文件写入失败，请检查后重新启动： {str(e)}')
+        return False
+    if status:
+        print('配置文件写入完成...')
+    return True
+
+
+def update_system(local_version, new_version):
+    if not new_version:
+        return
+    print('开始备份数据...')
+    try:
+        backup_flag = backup_data(local_version)
+    except Exception as e:
+        print(e)
+        backup_flag = False
+    if not backup_flag:
+        skip_backup = input('备份数据失败，是否跳过备份直接更新：y/n?')
+        if isinstance(skip_backup, str) and skip_backup.startswith('y'):
+            print('开始更新...')
+        else:
+            return
+    status = replace_sample(new_version)
+    if not status:
+        return
+    restart_system(True)
+    result = run_update_scripts()
+    install_check = check_system()
+    if not result or not install_check:
+        restore_data(local_version)
+        restart_system()
+    else:
+        with open('.version', 'w') as f:
+            f.write(new_version)
+        print('更新完成~')
+        remove_images(local_version)
+    # if install_check:
+    #     local_version, new_version = github_download()
+    #
+    # else:
+    #     print('Evolute服务未安装或未启动...')
+    #     install_permission = input("是否进行更新重启：y/n？")
+    #     if isinstance(install_permission, str) and install_permission.startswith('y'):
+    #         run_system()
+    #     else:
+    #         print('退出...')
+
+
+def run_system(local_version, new_version):
+    run_install_scripts()
+    print('开始启动...')
+    if not new_version:
+        skip_permission = input("获取版本信息失败，是否跳过此次更新直接启动：y/n？")
+        if isinstance(skip_permission, str) and skip_permission.startswith('y'):
+            print('...')
+        else:
+            return
+    status = replace_sample(new_version)
+    if not status:
+        return
+    restart_system(False)
+    with open('.version', 'w') as f:
+        f.write(new_version)
+    print('启动成功~')
+
+    # # 检查一下是否已经有在启动
+    # install_check = check_system()
+    # if not install_check:
+    #     run_install_scripts()
+    #     print('开始启动...')
+    #     local_version, new_version = github_download()
+    #     if not new_version:
+    #         skip_permission = input("获取版本信息失败，是否跳过此次更新直接启动：y/n？")
+    #         if isinstance(skip_permission, str) and skip_permission.startswith('y'):
+    #             print('...')
+    #         else:
+    #             return
+    #     status = replace_sample(new_version)
+    #     if not status:
+    #         return
+    #     importlib.reload(evolute_params)
+    #     evolute_params.restart_system(False)
+    #     with open('.version', 'w') as f:
+    #         f.write(new_version)
+    #     print('启动成功~')
+    # else:
+    #     update_flag = input('检测到您本地已安装相关服务，是否进行更新：y/n?')
+    #     if isinstance(update_flag, str) and update_flag.startswith('y'):
+    #         print('开始更新...')
+    #         update_system()
+    #     print('退出...')
+    #     return
+
+
+def rollback_system():
+    local_version = ''
+    if os.path.exists('./.version'):
+        with open('.version', 'r') as f:
+            local_version = f.readline()
+    restore_data(local_version)
+    print('回退到上个版本...')
+    restart_system()
+
+
+def kill_system():
+    os.system(f'docker-compose down')
+
+
+def system_status():
+    res = os.popen('docker-compose ps')
+    if not res:
+        print('服务未启动~')
+        return
+    for line in res.readlines():
+        print(line)
 
 
 if __name__ == '__main__':
